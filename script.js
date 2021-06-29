a0f199dfe57288a5fd95253cd0f9c78810fc1455
Sasun MKrtumyan <sasunmkrtumyan92@gmail.com>
2021-06-29 23:31:54 +0400

    task: finished, great job

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const search = document.querySelector(".input");
 const button = document.querySelector(".btn");
 const body = document.querySelector(".body");
+const defined = document.querySelector(".not-found");
+
+var insertedContent = document.querySelector(".insertedContent");
 button.addEventListener("submit", (e) => {
   e.preventDefault();
 });
@@ -17,20 +20,30 @@ function fetching(str) {
 }
 
 function render(data) {
+  const cards = document.querySelectorAll(".card");
+
+  if (cards.length) {
+    cards.forEach((card) => body.removeChild(card));
+  }
+  if (!data.length) {
+    defined.style.display = "block";
+  }
   data.forEach((el) => {
     const html = `
-            <div class="body"> 
+
                  <div class="card">
                     <img class="img" src="${el.flag}">
                     <h3>Name - ${el.name}</h3>
                     <h4>Capital - ${el.capital}</h4>
-                    <h4>Population - ${el.population/1000000} mln</h4>
+                    <h4>Population - ${(el.population / 1000000).toFixed(
+                      3
+                    )} mln</h4>
                     <h4>Area - ${el.area}</h4>
                     <h4>Region - ${el.region}</h4>
                 </div>
-            </div>
 
     `;
+
     body.insertAdjacentHTML("beforeEnd", html);
   });
 }
